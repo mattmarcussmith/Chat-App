ad18d2d442b8c7a202530b6afbdc5107911b41d9
Matthew Smith <matthewsmith@Matthews-MacBook-Pro.local>
2023-05-11 16:57:40 -0400

    first commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatApp.Hubs
+{
+	public class ChatHub : Hub
+	{
+		public async Task SendMessage(string userInput)
+		{
+			await Clients.All.SendAsync("ReceiveMessage", userInput);
+
+		}
+	}
+}
+
---
+++ wwwroot/js/chat.js
@@ -0,0 +1,36 @@
+﻿"use strict";
+
+//Inital connection to hub.
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub").build();
+
+//Disable the send button until connection is established.
+document.querySelector(".send-button").disabled = true;
+
+connection.on("ReceiveMessage", userInput => {    
+
+    var p = document.createElement("p");
+    var messageList = document.querySelector(".message-list").appendChild(p);
+   
+    messageList.textContent = `${userInput}`;
+    document.querySelector(".user-input").value = "";
+ 
+});
+
+connection.start().then(() => {
+    document.querySelector(".send-button").disabled = false;
+    
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.querySelector(".send-button")
+    .addEventListener("click", (event) => {
+    var userInput = document.querySelector(".user-input").value;
+ 
+    connection.invoke("SendMessage", userInput)
+        .catch((err) => {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
